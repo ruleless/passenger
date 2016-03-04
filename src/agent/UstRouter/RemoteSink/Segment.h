93fe5099f95f8a9077200ee5e83b197e12498721
Hongli Lai (Phusion) <hongli@phusion.nl>
2016-03-04 22:16:44 +0100

    ...

--- src/agent/UstRouter/RemoteSink/Segment.h
+++ src/agent/UstRouter/RemoteSink/Segment.h
@@ -1,33 +1,94 @@
-class Segment {
-public:
-	struct SegmenterFields {
-		bool toBeForwarded;
-		STAILQ_ENTRY(Segment) nextToBeForwarded;
-	};
+/*
+ *  Phusion Passenger - https://www.phusionpassenger.com/
+ *  Copyright (c) 2016 Phusion Holding B.V.
+ *
+ *  "Passenger", "Phusion Passenger" and "Union Station" are registered
+ *  trademarks of Phusion Holding B.V.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+#ifndef _PASSENGER_UST_ROUTER_REMOTE_SINK_SEGMENT_H_
+#define _PASSENGER_UST_ROUTER_REMOTE_SINK_SEGMENT_H_
 
-	struct BatcherFields {
-		TransactionList queue;
-		size_t bytesAdded;
-		size_t bytesQueued;
-		size_t bytesProcessing;
-		unsigned long long lastQueueAddTime;
-		unsigned long long lastProcessingBeginTime;
-		unsigned long long lastProcessingEndTime;
-		unsigned int nQueued;
-		unsigned int nProcessing;
-	};
+#include <boost/container/small_vector.hpp>
+#include <cstddef>
+#include <psg_sysqueue.h>
+#include <UstRouter/Transaction.h>
 
-	struct SenderFields {
-		typedef boost::container::small_vector<4, ServerPtr> SmallServerList;
-		string serversHash;
+namespace Passenger {
+namespace UstRouter {
+namespace RemoteSink {
 
-		SmallServerList servers;
-		SmallServerList balancingList;
-		unsigned int nextServer;
-		bool allHealthy;
-	};
+using namespace std;
 
-	SegmenterFields segmenterFields;
-	BatcherFields batcherFields;
-	SenderFields senderFields;
+
+struct Segment {
+	typedef boost::container::small_vector<4, ServerPtr> SmallServerList;
+
+	/****** Fields used by Segmenter ******/
+
+	STAILQ_ENTRY(Segment) nextInSegmenterList;
+	string segmentKey;
+	bool scheduledForBatching;
+
+
+	/****** Fields used by Segmenter and Batcher ******/
+
+	// Linked list of all segments that a Batcher::add() call
+	// should process.
+	STAILQ_ENTRY(Segment) nextScheduledForBatching;
+
+	// List of transactions, provided by the Segmenter, to
+	// be batched by the Batcher.
+	TransactionList incomingTransactions;
+	size_t bytesIncomingTransactions;
+	unsigned int nIncomingTransactions;
+
+
+	/****** Fields used by Batcher ******/
+
+	TransactionList queue;
+	size_t bytesAdded;
+	size_t bytesQueued;
+	size_t bytesProcessing;
+	unsigned long long lastQueueAddTime;
+	unsigned long long lastProcessingBeginTime;
+	unsigned long long lastProcessingEndTime;
+	unsigned int nQueued;
+	unsigned int nProcessing;
+
+
+	/****** Fields used by Segmenter and Sender ******/
+
+	SmallServerList servers;
+
+
+	/****** Fields used by Sender *******/
+
+	SmallServerList balancingList;
+	unsigned int nextServer;
+	bool allHealthy;
 };
+
+
+} // namespace RemoteSink
+} // namespace UstRouter
+} // namespace Passenger
+
+#endif /* _PASSENGER_UST_ROUTER_REMOTE_SINK_SEGMENT_H_ */
--- src/agent/UstRouter/RemoteSink/Segmenter.h
+++ src/agent/UstRouter/RemoteSink/Segmenter.h
@@ -1,3 +1,9 @@
+class AbstractBatcher {
+public:
+	~AbstractBatcher();
+	virtual void schedule(SegmentList &segments) = 0;
+};
+
 class Segmenter {
 private:
 	class TransferInfo: public CurlLibevIntegration::TransferInfo {
@@ -26,7 +32,7 @@ private:
 			}
 			STAILQ_REMOVE(&self->transferInfos, this, TransferInfo, next);
 			curl_easy_cleanup(curl);
-			self->keyInfoApiLookupFinished(key, code, httpCode,
+			self->apiLookupFinished(key, code, httpCode,
 				responseBody, errorBuffer);
 			delete this;
 		}
@@ -34,15 +40,19 @@ private:
 
 	class KeyInfo {
 		SegmentPtr segment;
-		ev_tstamp lastCheckTime;
+		string key;
+		ev_tstamp lastRefreshSuccessTime;
+		ev_tstamp lastRefreshErrorTime;
 		ev_tstamp lastRejectionErrorTime;
 		unsigned int recheckTimeoutWhenAllHealthy;
 		unsigned int recheckTimeoutWhenHaveErrors;
 		bool rejectionActive;
 		bool lookingUp;
 
-		KeyInfo()
-			: lastCheckTime(0),
+		KeyInfo(const string &_key)
+			: key(_key),
+			  lastRefreshSuccessTime(0),
+			  lastRefreshErrorTime(0),
 			  lastRejectionErrorTime(0),
 			  recheckTimeoutWhenAllHealthy(5 * 60),
 			  recheckTimeoutWhenHaveErrors(60),
@@ -60,6 +70,7 @@ private:
 	SegmentList segments;
 	StringKeyTable<KeyInfoPtr> keyInfos;
 	TransferInfoList transferInfos;
+	AbstractBatcher *batcher;
 	struct ev_timer timer;
 	size_t peakSize;
 
@@ -70,8 +81,8 @@ private:
 		if (keyInfos.lookup(key, &keyInfo)) {
 			return *keyInfo;
 		} else {
-			KeyInfoPtr newKeyInfo(make_shared<KeyInfo>());
-			if (initiateApiLookup(newKeyInfo, key)) {
+			KeyInfoPtr newKeyInfo(make_shared<KeyInfo>(key));
+			if (initiateApiLookup(newKeyInfo)) {
 				keyInfos.insert(key, newKeyInfo);
 				STAILQ_INSERT_TAIL(&unknownKeyInfos, newKeyInfo.get(), next);
 				return newKeyInfo;
@@ -84,21 +95,284 @@ private:
 	void forwardToBatcher(SegmentList &segments) {
 		Segment *segment, *nextSegment;
 
-		STAILQ_FOREACH(segment, &segments, segmenterFields.nextToBeForwarded) {
-			segment->segmenterFields.toBeForwarded = false;
+		STAILQ_FOREACH(segment, &segments, nextScheduledForBatching) {
+			segment->scheduledForBatching = false;
 		}
 
 		batcher.schedule(segments);
+		assert(STAILQ_EMPTY(&segments->incomingTransactions));
 
-		STAILQ_FOREACH_SAFE(segment, &segments, segmenterFields.nextToBeForwarded, nextSegment) {
-			STAILQ_NEXT(segment, segmenterFields.nextToBeForwarded) = NULL;
+		STAILQ_FOREACH_SAFE(segment, &segments, nextScheduledForBatching, nextSegment) {
+			STAILQ_NEXT(segment, scheduledForBatching) = NULL;
 		}
 
 		STAILQ_INIT(&segments);
 	}
 
+	void forwardToBatcher(Segment *segment) {
+		SegmentList segments;
+		STAILQ_INIT(&segments);
+		STAILQ_INSERT_TAIL(&segments, segment, nextScheduledForBatching);
+		batcher.schedule(segments);
+		assert(STAILQ_EMPTY(&segments->incomingTransactions));
+	}
+
 	void rescheduleNextKeyInfoRefresh() {
-		// TODO
+		ev_tstamp nextTimeout = std::numeric_limits<ev_tstamp>::max();
+		StringKeyTable<KeyInfoPtr>::ConstIterator it(&keyInfos);
+
+		while (*it != NULL) {
+			const KeyInfoPtr &keyInfo = it.getValue();
+
+			if (keyInfo->lookingUp) {
+				it.next();
+				continue;
+			}
+
+			if (keyInfo->rejectionActive) {
+				nextTimeout = std::min(nextTime, keyInfo->lastRejectionErrorTime
+					+ keyInfo->recheckTimeoutWhenHaveErrors);
+			} else {
+				nextTimeout = std::min(nextTime, keyInfo->lastCheckTime
+					+ keyInfo->recheckTimeoutWhenAllHealthy);
+			}
+
+			it.next();
+		}
+
+		if (nextTimeout == this->nextTimeout) {
+			return;
+		}
+
+		if (nextTime == std::numeric_limits<ev_tstamp>::max()) {
+			ev_timer_stop(loop, &timer);
+		} else {
+			ev_timer_stop(loop, &timer);
+			ev_timer_set(&timer, nextTime - ev_now(loop), 0);
+			ev_timer_start(loop, &timer);
+		}
+		this->nextTimeout = nextTimeout;
+	}
+
+	string createSegmentKey(const Json::Value &doc) {
+		return stringifyJson(doc["targets"]);
+	}
+
+	Segment *findSegment(const StaticString &segmentKey) {
+		Segment *segment;
+
+		STAILQ_FOREACH(segment, &segments, nextInSegmenterList) {
+			if (segmentKey == segment->segmentKey) {
+				return segment;
+			}
+		}
+
+		return NULL;
+	}
+
+	bool validateApiResponse(const Json::Value &doc) const {
+		if (OXT_UNLIKELY(!doc.isObject())) {
+			return false;
+		}
+		if (OXT_UNLIKELY(doc.isMember("status"))) {
+			return false;
+		}
+		if (OXT_LIKELY(doc["status"].asString() == "ok")) {
+			if (OXT_UNLIKELY(!doc.isMember("targets") || !doc["targets"].isArray())) {
+				return false;
+			}
+
+			Json::Value::const_iterator it, end = doc["targets"].begin();
+			for (it = doc["targets"].begin(); it != end; it++) {
+				const Json::Value &target = *it;
+				if (OXT_UNLIKELY(!target.isObject())) {
+					return false;
+				}
+				if (OXT_UNLIKELY(!target.isMember("base_url") || !target["base_url"].isString())) {
+					return false;
+				}
+				if (OXT_UNLIKELY(!target.isMember("weight") || !target["weight"].isUInt())) {
+					return false;
+				}
+				if (target["weight"].asUInt() == 0) {
+					return false;
+				}
+			}
+		} else if (doc["status"].asString() == "error") {
+			if (!doc.isMember("message") || !doc["message"].isString()) {
+				return false;
+			}
+			if (doc.isMember("retry_in") && !doc["retry_in"].isUInt()) {
+				return false;
+			}
+			if (doc.isMember("error_id") && !doc["error_id"].isString()) {
+				return false;
+			}
+		} else {
+			return false;
+		}
+
+		return true;
+	}
+
+	void updateSegmentInfo(Segment *segment, const Json::Value &doc) {
+		segment->lastCheckTime = ev_now(loop);
+		if (doc.isMember("retry_in")) {
+			segment->recheckTimeoutWhenAllHealthy = getJsonUintField(
+				doc["retry_in"], "all_healthy",
+				segment->recheckTimeoutWhenAllHealthy);
+			segment->recheckTimeoutWhenHaveErrors = getJsonUintField(
+				doc["retry_in"], "has_errors",
+				segment->recheckTimeoutWhenHaveErrors);
+		}
+	}
+
+	void handleApiResponse(const string &keyInfo, long httpCode,
+		const string &body)
+	{
+		Json::Reader reader;
+		Json::Value doc;
+
+		if (OXT_UNLIKELY(!reader.parse(body, doc, false))) {
+			handleApiResponseParseError(keyInfo, httpCode, body,
+				reader.getFormattedErrorMessages());
+			return;
+		}
+		if (OXT_UNLIKELY(!validateApiResponse(doc))) {
+			handleApiResponseInvalid(keyInfo, httpCode, body);
+			return;
+		}
+		if (OXT_UNLIKELY(doc["status"].asString() != "ok")) {
+			handleApiResponseErrorMessage(keyInfo, doc);
+			return;
+		}
+		if (OXT_UNLIKELY(httpCode != 200)) {
+			handleApiResponseInvalidHttpCode(keyInfo, httpCode, body);
+			return;
+		}
+
+		handleApiSuccessResponse(keyInfo, doc);
+	}
+
+	void handleApiResponseParseError(const KeyInfoPtr &keyInfo, unsigned int httpCode,
+		const string &body, const string &parseErrorMessage)
+	{
+		keyInfo->lastRefreshErrorTime = ev_now(loop);
+		setApiLookupError(
+			"Unable to fetch a list of Union Station gateway servers. "
+			"The Union Station load balancing server " + manifestURL
+			+ " returned an invalid response (unparseable). "
+			+ "Parse error: " + parseErrorMessage
+			+ "; key: " + keyInfo->key,
+			+ "; HTTP code: " + toString(httpCode)
+			+ "; body: \"" + cEscapeString(body) + "\""));
+	}
+
+	void handleApiResponseInvalid(const KeyInfoPtr &keyInfo, unsigned int httpCode,
+		const string &body)
+	{
+		keyInfo->lastRefreshErrorTime = ev_now(loop);
+		setApiLookupError(
+			"Unable to fetch a list of Union Station gateway servers. "
+			"The Union Station load balancing server " + manifestURL
+			+ " returned a invalid response (parseable, but does not comply to expected structure)."
+			+ " Key: " + keyInfo->key,
+			+ "; HTTP code: " + toString(httpCode)
+			+ "; body: \"" + cEscapeString(body) + "\""));
+	}
+
+	void handleApiResponseErrorMessage(const KeyInfoPtr &keyInfo,
+		const Json::Value &doc)
+	{
+		P_ASSERT_EQ(doc["status"].asString(), "error");
+
+		string message = "Unable to fetch a list of Union Station gateway servers. "
+			"The Union Station load balancing server " + docURL
+			+ " returned an error. Message from server: "
+			+ doc["message"].asString()
+			+ "; key: " + keyInfo->key;
+		if (doc.isMember("error_id")) {
+			message.append("; error ID: ");
+			message.append(doc["error_id"].asString());
+		}
+		setApiLookupError(message);
+
+		keyInfo->lastRefreshErrorTime = ev_now(loop);
+		if (doc.isMember("retry_in")) {
+			keyInfo->recheckTimeoutWhenHaveErrors = doc["retry_in"].asUInt();
+		}
+	}
+
+	void handleApiResponseInvalidHttpCode(const KeyInfoPtr &keyInfo,
+		unsigned int httpCode, const string &body)
+	{
+		keyInfo->lastRefreshErrorTime = ev_now(loop);
+		setApiLookupError(
+			"Unable to fetch a list of Union Station gateway servers. "
+			"The Union Station load balancing server " + manifestURL
+			+ " returned a invalid response."
+			+ " Key: " + keyInfo->key,
+			+ "; HTTP code: " + toString(httpCode)
+			+ "; body: \"" + cEscapeString(body) + "\""));
+	}
+
+	void handleApiSuccessResponse(const string &key, const Json::Value &doc) {
+		KeyInfoPtr keyInfo(keyInfos.lookupCopy(key));
+		assert(keyInfos != NULL);
+		string segmentKey(createSegmentKey(doc));
+		Segment *segment;
+
+		keyInfo->lastRefreshSuccessTime = ev_now(loop);
+
+		if (keyInfo->segment == NULL) {
+			// Create new segment
+			segment = new Segment(segmentKey);
+			updateSegmentInfo(segment, doc);
+			keyInfo->segment.reset(segment);
+			STAILQ_INSERT_TAIL(&segments, segment, nextInSegmenterList);
+
+			// Move all queued transactions with the current key
+			// into this segment
+			Transaction *transaction, *nextTransaction;
+			STAILQ_FOREACH_SAFE(&queue, transaction, next, nextTransaction) {
+				if (transaction->getUnionStationKey() == key) {
+					STAILQ_REMOVE(&queue, transaction, Transaction, next);
+					bytesQueued -= transaction->getBody().size();
+					nQueued--;
+
+					STAILQ_INSERT_TAIL(&segment->incomingTransactions,
+						transaction, next);
+					segment->bytesIncomingTransactions +=
+						transaction->getBody().size();
+					segment->nIncomingTransactions++;
+				}
+			}
+
+			forwardToBatcher(segment);
+
+		} else if (segmentKey != keyInfo->segment->segmentKey) {
+			// Move key to another segment
+			segment = findSegment(segmentKey);
+			if (segment == NULL) {
+				segment = new Segment(segmentKey);
+				STAILQ_INSERT_TAIL(&segments, segment, nextInSegmenterList);
+			}
+			updateSegmentInfo(segment, doc);
+			keyInfo->segment.reset(segment);
+
+		} else {
+			updateSegmentInfo(keyInfo->segment, doc);
+		}
+	}
+
+	void handleApiLookupPerformError(const KeyInfoPtr &keyInfo,
+		CURLcode code, const char *errorBuffer)
+	{
+		setApiLookupError(
+			"Unable to fetch a list of Union Station gateway servers. "
+			"The Union Station load balancing server " + manifestURL
+			+ " appears to be down. Error message: "
+			+ errorBuffer);
 	}
 
 	static size_t curlDataReceived(char *ptr, size_t size, size_t nmemb, void *userdata) {
@@ -112,7 +386,7 @@ private:
 	}
 
 protected:
-	virtual bool initiateApiLookup(const KeyInfoPtr &keyInfo, const string &key) {
+	virtual bool initiateApiLookup(const KeyInfoPtr &keyInfo) {
 		TransferInfo *transferInfo;
 		CURL *curl;
 
@@ -121,8 +395,8 @@ protected:
 			return false;
 		}
 
-		transferInfo = new TransferInfo(this, curl, key);
-		curl_easy_setopt(curl, CURLOPT_URL, apiUrl);
+		transferInfo = new TransferInfo(this, curl, keyInfo->key);
+		curl_easy_setopt(curl, CURLOPT_URL, manifestURL.c_str());
 		curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, (long) CURL_HTTP_VERSION_2);
 		curl_easy_setopt(curl, CURLOPT_PIPEWAIT, (long) 1);
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, (long) 1);
@@ -151,13 +425,14 @@ public:
 		: keys(2, CACHE_LINE_SIZE - ESTIMATED_MALLOC_OVERHEAD)
 	{
 		multi = curl_multi_init();
+		curl_multi_setopt(multi, CURLMOPT_PIPELINING, (long) CURLPIPE_MULTIPLEX);
 		curlLibevIntegration.initialize(loop, multi);
 	}
 
 	~Segmenter() {
 		Segment *segment, *nextSegment;
 
-		STAILQ_FOREACH_SAFE(segment, &segments, segmenterFields.next, nextSegment) {
+		STAILQ_FOREACH_SAFE(segment, &segments, nextInSegmenterList, nextSegment) {
 			segment->unref();
 		}
 
@@ -195,9 +470,9 @@ public:
 				segment->nIncomingTransactions++;
 				STAILQ_INSERT_TAIL(&segment->incomingTransactions, transaction, next);
 
-				if (!segment->segmenterFields.toBeForwarded) {
-					segment->segmenterFields.toBeForwarded = true;
-					STAILQ_INSERT_TAIL(&segmentsToForward, segment, segmenterFields.nextToBeForwarded);
+				if (!segment->scheduledForBatching) {
+					segment->scheduledForBatching = true;
+					STAILQ_INSERT_TAIL(&segmentsToForward, segment, nextScheduledForBatching);
 				}
 
 				bytesForwarded += transaction->getBody().size();
@@ -215,6 +490,8 @@ public:
 		}
 
 		if (nAdded != count) {
+			assert(bytesQueued > limit);
+			assert(totalBodySize > bytesAdded);
 			bytesDropped += totalBodySize - bytesAdded;
 			nDropped += count - nAdded;
 			P_WARN("Unable to lookup Union Station key information quickly enough. "
@@ -223,22 +500,24 @@ public:
 		}
 
 		forwardToBatcher(segments);
+
 		if (shouldRescheduleNextKeyInfoRefresh) {
 			rescheduleNextKeyInfoRefresh();
 		}
 	}
 
-	void keyInfoApiLookupFinished(const string &key, CURLcode code, long httpCode,
+	void apiLookupFinished(const string &key, CURLcode code, long httpCode,
 		const string &body, const char *errorBuffer)
 	{
-		handleKeyInfoApiLookupSuccessResponse(key, doc);
-	}
+		KeyInfoPtr keyInfo(keys.lookupCopy(key));
+		assert(keyInfo != NULL);
 
-	void handleKeyInfoApiLookupSuccessResponse(const string &key, const Json::Value &doc) {
-		SegmentList segments;
-		Segment;
+		keyInfo->lookingUp = false;
 
-		SEGMENTS
-		batcher.schedule(segments);
+		if (code == CURLE_OK) {
+			handleApiResponse(keyInfo, httpCode, body);
+		} else {
+			handleApiLookupPerformError(keyInfo, code, errorBuffer);
+		}
 	}
 };
